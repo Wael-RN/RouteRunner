b4e3f5914efafb7ad492b610ec40b20d34492c49
wael_naamah <wael.naamah@gmail.com>
2023-01-29 16:10:34 +0400

    set up crashes and anayltics

--- App.tsx
+++ App.tsx
@@ -15,15 +15,14 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
+  Alert,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -55,44 +54,53 @@ function Section({children, title}: SectionProps): JSX.Element {
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default class App extends React.Component {
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    this.checkPreviousSession();
+  }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Welcome to Route-Runner app, let's build amazing things!.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  async checkPreviousSession() {
+    const didCrashed = await Crashes.hasCrashedInLastSession();
+    if (didCrashed) {
+      // const report = await Crashes.lastSessionCrashReport(); // use this report to know more about the crash
+      Alert.alert("Sorry about that crash, we're working on a solution");
+    }
+  }
+
+  render() {
+    const backgroundStyle = {
+      backgroundColor: Colors.lighter,
+    };
+
+    return (
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={'light-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <View
+            style={{
+              backgroundColor: Colors.white,
+            }}>
+            <Section title="Step One">
+              Welcome to Route-Runner app, let's build amazing things!.
+              <Button
+                title="track event"
+                onPress={() => {
+                  Analytics.trackEvent('My custom event', {Internet: 'WIFI'});
+                }}
+              />
+            </Section>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -109,9 +117,4 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '400',
   },
-  highlight: {
-    fontWeight: '700',
-  },
 });
-
-export default App;
